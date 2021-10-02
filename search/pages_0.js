694b434d4cbf13a1feb03b78a7d9c456709c23a6
berviantoleo <berviantoleo@users.noreply.github.com>
2021-10-02 01:44:07 +0000

    deploy: 0b37578eed75b9c0f7e56d5065458b91e6f4a166

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['bervproject_2evalidation_2ecommon_15',['BervProject.Validation.Common',['../md_README.html',1,'']]]
+  ['bervproject_2evalidation_2ecommon_15',['BervProject.Validation.Common',['../md_BervProject_Validation_Common_README.html',1,'(Global Namespace)'],['../md_README.html',1,'(Global Namespace)']]]
 ];
